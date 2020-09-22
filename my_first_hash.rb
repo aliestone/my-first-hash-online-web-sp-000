eab7cef896e08f53262af41c907ced853781c2c0
Alie Stone <helloaliestone@gmail.com>
2020-09-22 17:33:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,10 @@
 def my_hash
-  {"my_name" => "Alie", "my_age" => "33"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  {"my_name"=> "Alie", "my_age" => "33"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
-  
+
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+  {"the_manifest"=>"items", "Whale_bone_corsets" => "5", "porcelain_vases"=> "2", "oil_paintings"=> "3"}# set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
