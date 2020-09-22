b6ba289411eaf21cf69fb0bb15d433e1f520ba8e
Alie Stone <helloaliestone@gmail.com>
2020-09-22 17:56:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,8 +18,10 @@ def retrieval
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
-  shipping_manifest["muskets"] = "2"
-  shipping_manifest["gun powder"] = "4"
+    shipping_manifest["muskets"] = "2"
+    shipping_manifest["gun powder"] = "4"
   puts shipping_manifest
+    end
   end
 end
+
